ad26d5e83e763aa87f93609345dde950683b45b1
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2024-12-31 01:12:58 -0300

    frontend: Home Page. reviews

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
 import { Check, Star } from "lucide-react";
 import Image from "next/image";
+import { Icons } from "@/components/Icons";
 
 export default function Home() {
   return (
@@ -112,6 +113,98 @@ export default function Home() {
                   className="w-64"
                   imgSrc="/testimonials/dogo-argentino.jpg"
                 />
+              </div>
+            </div>
+          </MaxWidthWrapper>
+        </section>
+
+        {/* value proposition section */}
+        <section className="bg-slate-100 py-24">
+          <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+            <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+              <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                What our{" "}
+                <span className="relative px-2">
+                  customers{" "}
+                  <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-5 md:-bottom-8 w-64 md:w-80 text-custom-green" />
+                </span>{" "}
+                say
+              </h2>
+              <img src="/snake-2.png" className="w-24 order-0 lg:order-2" />
+            </div>
+
+            <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+              {/* first user review */}
+              <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+                <div className="flex gap-0.5 mb-2">
+                  <Star className="h-5 w-5 text-custom-green fill-custom-green" />
+                  <Star className="h-5 w-5 text-custom-green fill-custom-green" />
+                  <Star className="h-5 w-5 text-custom-green fill-custom-green" />
+                  <Star className="h-5 w-5 text-custom-green fill-custom-green" />
+                  <Star className="h-5 w-5 text-custom-green fill-custom-green" />
+                </div>
+                <div className="text-lg leading-8">
+                  <p>
+                    &quot;The case feels{" "}
+                    <span className="p-05 bg-slate-800 text-white">
+                      durable
+                    </span>{" "}
+                    and I got various compliments on the design. Had the case
+                    for fourth months now and the{" "}
+                    <span className="p-05 bg-slate-800 text-white">
+                      image is still super clear
+                    </span>
+                    . Love it!&quot;
+                  </p>
+                </div>
+                <div className="flex gap-4 mt-2">
+                  <img
+                    className="rounded-full h-12 w-12 object-cover"
+                    src="/users/user-6.png"
+                  />
+                  <div className="flex flex-col">
+                    <p className="font-semibold">Luck Ra</p>
+                    <div className="flex gap-1.5 items-center text-zinc-600">
+                      <Check className="h-4 w-4 stroke-[3px] text-custom-green" />
+                      <p className="text-sm">Verified Purchase</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {/* second user review */}
+              <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+                <div className="flex gap-0.5 mb-2">
+                  <Star className="h-5 w-5 text-custom-green fill-custom-green" />
+                  <Star className="h-5 w-5 text-custom-green fill-custom-green" />
+                  <Star className="h-5 w-5 text-custom-green fill-custom-green" />
+                  <Star className="h-5 w-5 text-custom-green fill-custom-green" />
+                  <Star className="h-5 w-5 text-custom-green fill-custom-green" />
+                </div>
+                <div className="text-lg leading-8">
+                  <p>
+                    &quot;I recently purchased this phone case for my iPhone 23,
+                    and I couldn't be happier with my decision. Overall, this
+                    phone case is an excellent combination of{" "}
+                    <span className="p-05 bg-slate-800 text-white">
+                      style and functionality
+                    </span>
+                    . Highly recommended!&quot;
+                  </p>
+                </div>
+                <div className="flex gap-4 mt-2">
+                  <img
+                    className="rounded-full h-12 w-12 object-cover"
+                    src="/users/user-7.png"
+                  />
+                  <div className="flex flex-col">
+                    <p className="font-semibold">Paulo Londra</p>
+                    <div className="flex gap-1.5 items-center text-zinc-600">
+                      <Check className="h-4 w-4 stroke-[3px] text-custom-green" />
+                      <p className="text-sm">Verified Purchase</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </MaxWidthWrapper>

03a994d6e90692c10a56b144ff16a2964dc60584
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2025-01-03 06:13:22 -0300

    fullstack: cropping and saving images

--- src/config/products.ts
+++ src/config/products.ts
@@ -1,10 +1,12 @@
 export const PRODUCT_PRICES = {
   material: {
     silicone: 0,
-    polycarbonate: 5_00,
+    polycarbonate: 2_00,
   },
   finish: {
     smooth: 0,
-    textured: 3_00,
+    textured: 1_00,
   },
 } as const;
+
+export const BASE_PRICE = 8_00;
